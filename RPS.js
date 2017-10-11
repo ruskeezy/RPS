597c55f547853b9bf1163bd94943fae8ead9027f
ruskeezy <31902365+ruskeezy@users.noreply.github.com>
2017-10-11 15:17:05 -0700

    Add files via upload

---
+++ RPS.js
@@ -0,0 +1,88 @@
+var playerScore = 0;
+var computerScore = 0;
+var playerChoice;
+var computerChoice = Math.floor(Math.random() *3);
+var choiceP = document.getElementById("choice");
+
+
+function showChoice(choise) {
+    paragraph.innerText = "You chose " + playerChoice + " !"
+    paragraph.innerText = "Computer chose " + computerChoice + " !"
+}
+function playerWins() {
+    paragraph.innerText = "You won!"
+    playerScore++
+}
+function computerWins() {
+    paragraph.innerText = "Computer won!"
+    computerScore++
+}
+function tie() {
+    paragraph.innerText = "It's a tie"
+}
+
+function playRPS() {
+
+
+    var computerChoice = Math.floor(Math.random() *3);
+
+    var choices = {
+        '0': 'Rock',
+        '1': 'Paper',
+        '2': 'Scissors'
+    };
+
+    computerChoice = choices[computerChoice];
+
+    if (playerChoice === 'Rock') {
+        if (computerChoice === 'Scissors') {
+            playerWins();
+        } else if(computerChoice === 'Paper') {
+            computerWins();
+        } else if(computerChoice === 'Rock')
+        tie();
+    }
+
+    if (playerChoice === 'Paper') {
+        if(computerChoice === 'Rock') {
+            playerWins();
+        } else if(computerChoice === 'Scissors') {
+            computerWins();
+        } else if(computerChoice === 'Paper')
+        tie();
+    }
+
+    if (playerChoice === 'Scissors') {
+        if (computerChoice === 'Paper') {
+            playerWins();
+        } else if(computerChoice === 'Rock') {
+            computerWins();
+        } else if(computerChoice === 'Scissors')
+        tie();
+    }
+}
+
+var choiceP = document.getElementById("choice");
+var paragraph = document.getElementById("result");
+var scoreP = document.getElementById("score");
+var rockIMG = document.getElementById("rock");
+var paperIMG = document.getElementById("paper");
+var scissorsIMG = document.getElementById("scissors");
+
+rockIMG.addEventListener("click", function() {
+    playerChoice = 'Rock'
+    playRPS();
+    scoreP.innerText = "Player score: " + playerScore + ' Computer score: ' + computerScore
+})
+
+paperIMG.addEventListener("click", function() {
+    playerChoice = 'Paper'
+    playRPS();
+    scoreP.innerText = "Player score: " + playerScore + ' Computer score: ' + computerScore
+})
+
+scissorsIMG.addEventListener("click", function() {
+    playerChoice = 'Scissors'
+    playRPS();
+    scoreP.innerText = "Player score: " + playerScore + ' Computer score: ' + computerScore
+})
